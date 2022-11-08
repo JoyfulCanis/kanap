2741d76921dd82b2ac1a9125027fc4e30f70fdc6
Neïl Aloulou <98472708+JoyfulCanis@users.noreply.github.com>
2022-11-08 12:40:39 +0100

    first commit

---
+++ back/article.js
@@ -0,0 +1,15 @@
+
+
+class Articles {
+    constructor(jsOnArticle){
+
+        this.id = jsOnArticle.id; //string
+        this.name = jsOnArticle.name; //string
+        this.price = jsOnArticle.price; //num
+        this.imageUrl = jsOnArticle.imageUrl; //string
+        this.description = jsOnArticle.description; //string
+        this.altTxt = jsOnArticle.altTxt; //string
+        this.colors = jsOnArticle.colors //array
+    }
+    
+}
